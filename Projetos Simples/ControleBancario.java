acb68e714798b0345873f00ba417e60839afecdd
Everett <gmonteiro0808@gmail.com>
2025-06-16 20:12:18 -0300

    Começando controle bancario

---
+++ Projetos Simples/ControleBancario.java
@@ -0,0 +1,13 @@
+public class ControleBancario {
+    public static void main(String[] args) {
+        String nome = "Arthur Morgan";
+        String tipoConta = "Corrente";
+        double saldo = 0.00;
+
+        System.out.println("************************\n");
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n************************");
+    }
+}
